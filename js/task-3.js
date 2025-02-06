a6f1409b16206c5da8ea5c283813ddbdb9870e9c
Vladislav Zarovniy <vladislavzarovnii13@gmail.com>
2025-02-06 15:06:11 +0200

    upd-2

--- js/task-3.js
+++ js/task-3.js
@@ -6,8 +6,7 @@ function getElementWidth(content, padding, border) {
 
 };
 
-console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
 
-console.log(getElementWidth("60px", "12px", "8.5px"));
-
-console.log(getElementWidth("200px", "0px", "0px"));
